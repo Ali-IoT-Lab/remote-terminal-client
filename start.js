8e8d0cad39e8d6f8a6b04897318a32f3659a9b40
忙僧2018 <156110386@qq.com>
2019-01-24 14:15:44 +0800

    守护脚本

--- start.js
+++ start.js
@@ -1,11 +1,22 @@
 const os = require("os");
 const fs = require("fs");
+const p = require("path");
 const child_process = require("child_process");
 const paths = require('./lib/paths.js');
 
 var newVersion = process.argv[2];
+var HOME="";
 
+if(os.platform() == 'linux') {
+  HOME='home'
+}
+if(os.platform() == 'darwin'){
+  HOME='Users'
+}
 
+if(fs.existsSync(p.join('/', HOME, 'moja', 'install-mode'))) { //curl安装方式判断
+  process.env.PATH=`${process.env.PATH}:/${HOME}/moja/nodejs/bin`;
+}
 
 var pm2Json=`
 {
