389a057ca747e36c570144b8ce2131f24e1ef97f
忙僧2018 <156110386@qq.com>
2019-01-26 16:03:58 +0800

    删除多余js文件

--- start.js
+++ start.js
@@ -34,7 +34,7 @@ var pm2Json=`
 //判断根据版本号判断当前启动是否为升级启动 若为升级起动则删除旧版本进程
 var oldVersion = fs.readFileSync(paths.MOJA_VERSION_PATH).toString().replace(/(\r|\n)/gi, "");
 if(oldVersion != newVersion) {
-  child_process.execSync(`sh ${path.resolve(path.join(__dirname, "./operation/killApp.sh"))} ${oldVersion}`);
+  child_process.execSync(`sh ${path.resolve(path.join(__dirname, "./operation/killApp.sh"))} ${newVersion}`);
 }
 
 var pm2JsonPath = `${os.homedir()}/.moja/client/v${newVersion}/node_modules/remote-terminal-client/pm2.json`;
