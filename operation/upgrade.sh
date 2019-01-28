5c65d945bfdc3b5c969e5f1e1008690cb9751533
Legendary <156110386@qq.com>
2019-01-28 12:36:25 +0800

    Update upgrade.sh

--- operation/upgrade.sh
+++ operation/upgrade.sh
@@ -6,7 +6,7 @@ clientVersion=$1
 
 mkdir -p ~/.moja/client/v$1
 echo "3"> ~/.moja/stage
-npm install remote-terminal-client@latest --unsafe-perm=true --registry=https://registry.cnpmjs.org --prefix ~/.moja/client/v$clientVersion
+npm install https://github.com/Ali-IoT-Lab/remote-terminal-client.git#develop --unsafe-perm=true --registry=https://registry.cnpmjs.org --prefix ~/.moja/client/v$clientVersion
 echo "4"> ~/.moja/stage
 node ~/.moja/client/v$clientVersion/node_modules/remote-terminal-client/start.js $clientVersion
 result=$?
