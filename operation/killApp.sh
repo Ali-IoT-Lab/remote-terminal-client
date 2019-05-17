ace8a7c0d9b9a083a1c63c58c9b3e1f67c3cdee3
王贵林 <wb-wgl427174@alibaba-inc.com>
2019-05-17 10:33:41 +0800

    版本号目录修改

--- operation/killApp.sh
+++ operation/killApp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 newClientVersion=$1
-oldVersion=`cat ~/.moja/moja-version|tr -d '\n'`
+oldVersion=`cat ~/.moja/mojaConfig/moja-version|tr -d '\n'`
 ~/.moja/pmtwo/node_modules/pm2/bin/pm2 delete client-v$oldVersion
-echo $newClientVersion > ~/.moja/moja-version
+echo $newClientVersion > ~/.moja/mojaConfig/moja-version
 rm -r -f ~/.moja/client/v$oldVersion
 echo > ~/.moja/stage
